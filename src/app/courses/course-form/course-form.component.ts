b04f52f6e815e97b8338ab1ce519d961619953b9
Odlavir Florentino <lavirandrade@gmail.com>
2023-02-14 14:57:23 -0300

    Iniciando o front na parte dos cursos novos

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.scss']
+  styleUrls: ['./course-form.component.scss'],
 })
-export class CourseFormComponent {
+export class CourseFormComponent implements OnInit {
+  form: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar
+  ) {
+    this.form = this.formBuilder.group({
+      name: [null],
+      category: [null],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    this.service.save(this.form.value).subscribe(
+      (data) => console.log(data),
+      (error) => this.onError()
+    );
+  }
+
+  onCancel() {
+    alert('Você acaba de clicar no botão de cancelar');
+  }
+
+  private onError() {
+    this.snackBar.open('Erro ao salvar curso.', '', {
+      duration: 3000,
+    });
+  }
 }
