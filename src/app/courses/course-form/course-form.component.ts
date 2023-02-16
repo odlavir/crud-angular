24f8c50fef0e7f0d2caf332041b2263be9493722
Odlavir Florentino <lavirandrade@gmail.com>
2023-02-16 16:03:32 -0300

    Finalizando a logica do botao salvar

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,6 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { NonNullableFormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { CoursesService } from './../services/courses.service';
@@ -10,30 +11,38 @@ import { CoursesService } from './../services/courses.service';
   styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent implements OnInit {
-  form: FormGroup;
+  form = this.formBuilder.group({
+    name: [''],
+    category: [''],
+  });
 
   constructor(
-    private formBuilder: FormBuilder,
+    private formBuilder: NonNullableFormBuilder,
     private service: CoursesService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private location: Location
   ) {
-    this.form = this.formBuilder.group({
-      name: [null],
-      category: [null],
-    });
+    // this.form;
   }
 
   ngOnInit(): void {}
 
   onSubmit() {
     this.service.save(this.form.value).subscribe(
-      (data) => console.log(data),
+      (data) => this.onSucess(),
       (error) => this.onError()
     );
   }
 
   onCancel() {
-    alert('Você acaba de clicar no botão de cancelar');
+    this.location.back();
+  }
+
+  private onSucess() {
+    this.snackBar.open('Curso salvo com sucesso!', '', {
+      duration: 3000,
+    });
+    this.onCancel();
   }
 
   private onError() {
